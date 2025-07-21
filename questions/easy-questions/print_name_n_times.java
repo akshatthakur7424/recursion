0efc15df4a984f9a12c84e6930cdece1343d0f0f
Akshat Singh Thakur <developerakshatsingh@gmail.com>
2025-07-20 22:52:29 -0700

    print_name_n_times problem

---
+++ questions/easy-questions/print_name_n_times.java
@@ -0,0 +1,22 @@
+public class print_name_n_times {
+    static int counter = 0;
+
+    // recursive function - print name n times
+    static void print_name( int n, String name ) {
+        // base condition 
+        if ( counter == n ) {
+            return;
+        }
+        
+        // priting name, incrementing counter and calling function again 
+        System.out.println(counter + ". " + name);
+        counter++;
+        print_name(n, name);
+    }
+
+    public static void main(String[] args) {
+        print_name(5, "Akshat Singh Thakur");
+    }
+}
+
+
